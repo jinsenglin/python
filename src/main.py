b572ccb12eb6e03d262a841253718aba09dc0807
Chun Cheng Lin <jinsenglin@users.noreply.github.com>
2016-03-17 23:11:13 +0800

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2.7                                                            
+# -*- coding: UTF-8 -*-                                                             
+                                                                                    
+                                                                                    
+def main():                                                                         
+    print "Main"                                                                    
+                                                                                    
+if __name__ == '__main__':                                                          
+    main()
