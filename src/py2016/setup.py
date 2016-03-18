da77ba93195496432ffec93300dd4cb905ccfa02
Chun Cheng Lin <jinsenglin@users.noreply.github.com>
2016-03-18 16:15:43 +0800

    Update setup.py

--- src/py2016/setup.py
+++ src/py2016/setup.py
@@ -3,12 +3,10 @@ from distutils.core import setup
 setup(
     name='py2016',
     packages=['pkg1', 'pkg2'],
-    scripts=['runner'],
     version='1.0.0',
     description='My first python project',
     author='jimlintw922',
     author_email='jimlintw922@gmail.com',
     url='https://github.com/jinsenglin/python/tree/master/src/py2016',
     keywords=['example'],
-    classifiers=[],
 )
