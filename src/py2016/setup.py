d41b29a48444694bd3a79f5642d0c37343795db3
Chun Cheng Lin <jinsenglin@users.noreply.github.com>
2016-03-18 16:05:08 +0800

    Update setup.py

--- src/py2016/setup.py
+++ src/py2016/setup.py
@@ -1,1 +1,15 @@
+from distutils.core import setup
 
+setup(
+    name='py2016',
+    packages=['pkg1', 'pkg2'],
+    scripts=['runner'],
+    version='1.0',
+    description='My first python project',
+    author='jimlintw922',
+    author_email='jimlintw922@gmail.com',
+    url='https://github.com/jinsenglin/python/tree/master/src/py2016',
+    download_url='https://github.com/jinsenglin/python/tarball/v1.0',
+    keywords=['example'],
+    classifiers=[],
+)
