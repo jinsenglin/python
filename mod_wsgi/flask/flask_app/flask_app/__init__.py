4212e3bb172977db9ae726dafe27bd61308ce564
Jim Lin <jimlintw922@gmail.com>
2017-10-19 18:23:36 +0800

    mod_wsgi/flask

---
+++ mod_wsgi/flask/flask_app/flask_app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+    return "Root"
+
+
+@app.route("/hello")
+def hello():
+    return "Hello"
+
+
+@app.route("/auth/always_deny")
+def auth_always_deny():
+    import json
+    obj = {}
+    obj["apiVersion"] = "authentication.k8s.io/v1beta1"
+    obj["kind"] = "TokenReview"
+    obj["status"] = {}
+    obj["status"]["authenticated"] = False
+    return json.dumps(obj)
+
+
+if __name__ == "__main__":
+    app.run()
