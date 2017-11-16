1fef34a11fdb3ad3445fca91417d6e7419f018f9
Jim Lin <jimlintw922@gmail.com>
2017-11-16 14:33:28 +0800

    add 0000-0000-0000-0000.os.yaml

--- apisvc/apisvc/main.py
+++ apisvc/apisvc/main.py
@@ -35,13 +35,23 @@ def k8s_cli():
 
 @app.route("/os/sdk")
 def os_sdk():
-    return "TODO"
+    # TODO utility to generate yaml file from openrc file
+    # TODO switch cloud
+    # TODO check thread-safe
+    import os_client_config
+    occ = os_client_config.OpenStackConfig()
+    cloud = occ.get_one_cloud('cc-iaas')
+    from openstack import connection
+    conn = connection.from_config(cloud_config=cloud)
+    for server in conn.compute.servers():
+        print(server.instance_name)
+    return "/os/sdk"
 
 @app.route("/k8s/sdk")
 def k8s_sdk():
     # TODO check thread-safe
     from kubernetes import client, config
-    config.load_kube_config(config_file='../samples/0000-0000-0000-0000.yaml')
+    config.load_kube_config(config_file='../samples/0000-0000-0000-0000.k8s.yaml')
     v1 = client.CoreV1Api()
     ret = v1.list_pod_for_all_namespaces(watch=False)
     for i in ret.items:
