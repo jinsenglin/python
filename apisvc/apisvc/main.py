bdff0332187d8cada2b2f5204b6bff5db91adfa0
Jim Lin <jimlintw922@gmail.com>
2017-11-16 11:38:19 +0800

    add TODO

--- apisvc/apisvc/main.py
+++ apisvc/apisvc/main.py
@@ -11,5 +11,29 @@ def sleep():
     time.sleep(10)
     return "/sleep"
 
+@app.route("/etcd/put")
+def etcd_put():
+    return "TODO"
+
+@app.route("/etcd/get")
+def etcd_get():
+    return "TODO"
+
+@app.route("/os/cli")
+def etcd_put():
+    return "TODO"
+
+@app.route("/k8s/cli")
+def etcd_get():
+    return "TODO"
+
+@app.route("/os/sdk")
+def etcd_put():
+    return "TODO"
+
+@app.route("/k8s/sdk")
+def etcd_get():
+    return "TODO"
+
 if __name__ == "__main__":
     app.run()
