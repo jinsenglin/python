ee63e8a83bd409c153d059cce7b65333f2c9ab68
Jim Lin <jimlintw922@gmail.com>
2017-11-16 15:48:50 +0800

    update requirements.txt

--- apisvc/apisvc/main.py
+++ apisvc/apisvc/main.py
@@ -3,27 +3,54 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    return "/"
+    return "GET /"
+
+@app.route("/", methods=["POST"])
+def post_root():
+    return "POST /"
+
+@app.route("/header")
+def header():
+    from flask import request
+    print(request.headers.get('X-PERSONATE'))
+    return "GET /header"
+
+from flask_restful import Resource, Api
+class TodoSimple(Resource):
+    def get(self, id):
+        print(id)
+        return "GET /resource"
+
+    def put(self, id):
+        print(id)
+        return "PUT /resource"
+
+    def delete(self, id):
+        print(id)
+        return "DELETE /resource"
+
+api = Api(app)
+api.add_resource(TodoSimple, '/resource/<string:id>')
 
 @app.route("/sleep")
 def sleep():
     import time
     time.sleep(10)
-    return "/sleep"
+    return "GET /sleep"
 
 @app.route("/etcd/put")
 def etcd_put():
     import etcd3
     etcd = etcd3.client(host='127.0.0.1')
     etcd.put('/foo', 'bar')
-    return "/etcd/put"
+    return "GET /etcd/put"
 
 @app.route("/etcd/get")
 def etcd_get():
     import etcd3
     etcd = etcd3.client(host='127.0.0.1')
     etcd.get('/foo')
-    return "/etcd/get"
+    return "GET /etcd/get"
 
 @app.route("/os/cli")
 def os_cli():
@@ -36,7 +63,6 @@ def k8s_cli():
 @app.route("/os/sdk")
 def os_sdk():
     # TODO utility to generate yaml file from openrc file
-    # TODO check thread-safe
     import os_client_config
     occ = os_client_config.OpenStackConfig(config_files=['../samples/0000-0000-0000-0000.os.yaml'])
     cloud = occ.get_one_cloud('cc-iaas')
@@ -44,18 +70,17 @@ def os_sdk():
     conn = connection.from_config(cloud_config=cloud)
     for server in conn.compute.servers():
         print(server.instance_name)
-    return "/os/sdk"
+    return "GET /os/sdk"
 
 @app.route("/k8s/sdk")
 def k8s_sdk():
-    # TODO check thread-safe
     from kubernetes import client, config
     config.load_kube_config(config_file='../samples/0000-0000-0000-0000.k8s.yaml')
     v1 = client.CoreV1Api()
     ret = v1.list_pod_for_all_namespaces(watch=False)
     for i in ret.items:
         print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-    return "/k8s/sdk"
+    return "GET /k8s/sdk"
 
 if __name__ == "__main__":
     app.run()
