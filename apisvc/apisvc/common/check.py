3e523ce2d61a54d12d018c7766aa03b202262900
Jim Lin <jimlintw922@gmail.com>
2017-11-20 11:51:33 +0800

    update common/check.py

--- apisvc/apisvc/common/check.py
+++ apisvc/apisvc/common/check.py
@@ -1,7 +1,16 @@
 import re
 from functools import wraps
 from flask import request, abort
-from apisvc import app
+
+def _check_account_existed(personation):
+    """
+        return True if the specified account exists in the local cache store
+        return True if the specified account exists in the remote persistent store
+        return False if all checks are not passed
+    """
+
+    # TODO
+    return False
 
 PERSONATE_ADMIN = 'ADMIN'
 PERSONATE_TENANT = 'TENANT'
@@ -11,7 +20,7 @@ def need_personate_header(role):
     """
         return HTTP status code 400 if there is no X-PERSONATE header in the request
         return HTTP status code 400 if there is no or wrong role specified in the value of X-PERSONATE header
-        return HTTP status code 400 if the specified account id does not exist in the system
+        return HTTP status code 400 if the specified account does not exist in the system
         expand **kwargs by injecting an object 'apisvc_res_manager' if all checks are passed
     """
     def need_personate_header_decorator(fn):
@@ -21,9 +30,7 @@ def need_personate_header(role):
             if 'X-PERSONATE' in request.headers:
                 personation = request.headers.get('X-PERSONATE')
                 if re.match(role, personation):
-                    # TODO check
-                    app.logger.debug('app.config["APISVC_CONFIG_CACHE_PATH"] = {0}'.format(app.config['APISVC_CONFIG_CACHE_PATH']))
-                    if True:
+                    if _check_account_existed(personation):
                         # TODO expand **kwargs
                         kwargs['apisvc_res_manager'] = 'TODO'
                         result = fn(*args, **kwargs)
