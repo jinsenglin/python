b7695708548f9c3db4cfb958eee4fad148733407
Jim Lin <jimlintw922@gmail.com>
2017-11-22 14:59:11 +0800

    update requirements.txt

--- apisvc/apisvc/managers/cia.py
+++ apisvc/apisvc/managers/cia.py
@@ -1,6 +1,10 @@
+import requests
+
+
 class Manager(object):
     def __init__(self):
         pass
 
     def demo(self):
-        pass
+        r = requests.get('https://api.github.com/events')
+        print(r)
