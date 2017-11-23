08ecf74d0486a522da53b98921874f3f67959825
Jim Lin <jimlintw922@gmail.com>
2017-11-23 14:29:16 +0800

    update log messages

--- apisvc/apisvc/common/profile.py
+++ apisvc/apisvc/common/profile.py
@@ -6,9 +6,10 @@ from apisvc.common.log import LOGGER
 def timeit(fn):
     @wraps(fn)
     def wrapper(*args, **kw):
+        LOGGER.debug('api %r (%r, %r) starting' % (fn.__name__, args, kw))
         ts = time.time()
         result = fn(*args, **kw)
         te = time.time()
-        LOGGER.debug('%r (%r, %r) %2.2f sec' % (fn.__name__, args, kw, te - ts))
+        LOGGER.debug('api %r (%r, %r) finished of execution time = %2.2f sec' % (fn.__name__, args, kw, te - ts))
         return result
     return wrapper
