9edb7153371a494cc51f134b9764a1af063dd328
Jim Lin <jimlintw922@gmail.com>
2017-11-23 16:06:37 +0800

    update init-etcd-for-dev.sh

--- apisvc/scripts/init-etcd-for-dev.sh
+++ apisvc/scripts/init-etcd-for-dev.sh
@@ -3,6 +3,13 @@
 set -e
 
 export ETCDCTL_API=3
+
+etcdctl put /apisvc/controllers/k8s ok
+etcdctl put /apisvc/controllers/os ok
+
+etcdctl put /apisvc/nodes/comp1 ok
+etcdctl put /apisvc/nodes/comp2 ok
+
 etcdctl put /apisvc/accounts/0000-0000-0000-0000 ok
 cat ../samples/0000-0000-0000-0000.k8s.yaml | etcdctl put /apisvc/accounts/0000-0000-0000-0000/k8s
 cat ../samples/0000-0000-0000-0000.os.yaml | etcdctl put /apisvc/accounts/0000-0000-0000-0000/os
