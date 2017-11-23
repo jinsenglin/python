4488232ab0df722b4c0006609027da342c41f927
Jim Lin <jimlintw922@gmail.com>
2017-11-23 11:42:25 +0800

    fix init-fs-cache-for-dev.sh

--- apisvc/scripts/init-fs-cache-for-dev.sh
+++ apisvc/scripts/init-fs-cache-for-dev.sh
@@ -3,4 +3,4 @@
 set -e
 
 cp ../samples/0000-0000-0000-0000.k8s.yaml ../apisvc/cache/0000-0000-0000-0000.k8s.yaml
-cp ../samples/0000-0000-0000-0000.os.yaml ../apisvc/cache/0000-0000-0000-0000.k8s.yaml
+cp ../samples/0000-0000-0000-0000.os.yaml ../apisvc/cache/0000-0000-0000-0000.os.yaml
