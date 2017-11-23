af340c55b01a99ca98887dc8230602c8ee9e963f
Jim Lin <jimlintw922@gmail.com>
2017-11-23 16:48:04 +0800

    add func put_credential

--- apisvc/apisvc/common/db/etcd.py
+++ apisvc/apisvc/common/db/etcd.py
@@ -14,3 +14,9 @@ def get_account(account):
 def get_credential(account, target):
     value, key = _client.get('/apisvc/accounts/{0}/{1}'.format(account, target))
     return value, key
+
+
+def put_credential(account, credential_k8s, credential_os):
+    _client.put('/apisvc/accounts/{0}'.format(account), 'ok')
+    _client.put('/apisvc/accounts/{0}/k8s'.format(account), credential_k8s)
+    _client.put('/apisvc/accounts/{0}/os'.format(account), credential_os)
