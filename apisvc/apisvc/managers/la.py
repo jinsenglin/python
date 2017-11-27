f675ea611b63631cddfb18590e98293f27cb0fc8
Jim Lin <jimlintw922@gmail.com>
2017-11-27 15:17:11 +0800

    update mk-k8s-user-client-certificate-data.sh

--- apisvc/apisvc/managers/la.py
+++ apisvc/apisvc/managers/la.py
@@ -1,15 +1,33 @@
 import subprocess
+import json
+from apisvc.common.log import LOGGER
 
 
 class Manager(object):
     def __init__(self):
         pass
 
+    def mk_ks8_user_client_certificate_data(self, username, group='system:masters'):
+        try:
+            stdout = subprocess.check_output(['bash',
+                                              'shell/mk-k8s-user-client-certificate-data.sh',
+                                              '../../samples/ca.crt'
+                                              '../../samples/ca.key',
+                                              username,
+                                              group], shell=False)
+        except subprocess.CalledProcessError:
+            LOGGER.error('failed to make k8s user client certificate data')
+
+        data = json.loads(stdout)
+        LOGGER.debug('key = {0}'.format(data['key']))
+        LOGGER.debug('crt = {0}'.format(data['crt']))
+
     def demo(self):
         output = ''
 
         try:
-            output = subprocess.check_output(['ls', '-l', '../'])
+            output = subprocess.check_output('ls -l ../ | cat', shell=True)
+            output = subprocess.check_output(['ls', '-l', '../'], shell=False)
         except subprocess.CalledProcessError:
             # TODO
             pass
--- apisvc/apisvc/shell/mk-k8s-user-client-certificate-data.sh
+++ apisvc/apisvc/shell/mk-k8s-user-client-certificate-data.sh
@@ -3,6 +3,7 @@
 # package dependency
 # - openssl
 # - base64
+# - jq
 
 # sample usage
 # bash $0 ../../samples/ca.crt ../../samples/ca.key user1 role1
@@ -23,8 +24,13 @@ CRT=server.crt
 CRT_BASE64=server.crt.base64
 KEY_BASE64=server.key.base64
 
-openssl req -new -sha256 -keyout $KEY -out $CSR -days 3650 -newkey rsa:2048 -nodes -subj "/O=$O/CN=$CN"
-openssl x509 -req -days 3650 -sha1 -CA $CA_CRT  -CAkey $CA_KEY -CAcreateserial -in $CSR -out $CRT
+openssl req -new -sha256 -keyout $KEY -out $CSR -days 3650 -newkey rsa:2048 -nodes -subj "/O=$O/CN=$CN" > /dev/null 2>&1
+openssl x509 -req -days 3650 -sha1 -CA $CA_CRT  -CAkey $CA_KEY -CAcreateserial -in $CSR -out $CRT > /dev/null 2>&1
 
 base64 $CRT > $CRT_BASE64
 base64 $KEY > $KEY_BASE64
+
+jq -n "{
+    \"crt\": \"$(base64 $CRT)\",
+    \"key\": \"$(base64 $KEY)\"
+}"
