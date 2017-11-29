5ada7a04600b964552c204fa0e678619e737fa44
Jim Lin <jimlintw922@gmail.com>
2017-11-29 17:21:23 +0800

    refactoring v1 to v2

---
+++ apisvc/apisvc/routes/v2/admin/__init__.py
@@ -0,0 +1,19 @@
+from apisvc.common.route import ROUTE
+from apisvc.common.profile import timeit
+from apisvc.common.audit import audit_anonymous_access
+
+
+@ROUTE('/v2/admin/healthz')
+@timeit
+@audit_anonymous_access
+def v1_admin_healthz():
+    return 'ok', {'Content-Type': 'text/plain'}
+
+
+@ROUTE('/v2/admin/apis')
+@timeit
+@audit_anonymous_access
+def v1_admin_apis():
+    return {'status': 200,
+            'message': [{'method': 'GET', 'path': '/healthz'},
+                        {'method': 'GET', 'path': '/apis'}]}, {'Content-Type': 'application/json'}
