993a79e05f3f32dae23a842aee515b57e76f96da
Jim Lin <jimlintw922@gmail.com>
2017-11-29 17:15:32 +0800

    refactoring v1 to v2

---
+++ apisvc/apisvc/messages/v2/admin/nodes.py
@@ -0,0 +1,18 @@
+class Node(object):
+
+    def __init__(self):
+        self.input_for_get = {}
+        self.output_for_get = {'status': 200, 'result': []}
+
+        self.input_for_post = {}
+        self.output_for_post = {'status': 200, 'result': 'post'}
+
+        self.input_for_put = {}
+        self.output_for_put = {'status': 200, 'result': 'put'}
+
+        self.input_for_delete = {}
+        self.output_for_delete = {'status': 200, 'result': 'delete'}
+
+
+def new_message():
+    return Node()
---
+++ apisvc/apisvc/resources/v2/admin/nodes.py
@@ -0,0 +1,44 @@
+from flask_restful import Resource
+from apisvc.common.api import API
+from apisvc.common.profile import timeit
+from apisvc.common.audit import audit_access
+from apisvc.common import check
+from apisvc.handlers.v2.admin.nodes import new_handler
+from apisvc.messages.v2.admin.nodes import new_message
+
+
+class Tenant(Resource):
+    @timeit
+    @check.need_personate_header(check.PERSONATE_ADMIN)
+    @audit_access
+    def get(self, *args, **kwargs):
+        return new_handler().get(manager=kwargs['apisvc_res_manager'],
+                                 in_message=new_message().input_for_get,
+                                 out_message=new_message().output_for_get)
+
+    @timeit
+    @check.need_personate_header(check.PERSONATE_ADMIN)
+    @audit_access
+    def post(self, *args, **kwargs):
+        return new_handler().post(manager=kwargs['apisvc_res_manager'],
+                                  in_message=new_message().input_for_post,
+                                  out_message=new_message().output_for_post)
+
+    @timeit
+    @check.need_personate_header(check.PERSONATE_ADMIN)
+    @audit_access
+    def put(self, *args, **kwargs):
+        return new_handler().put(manager=kwargs['apisvc_res_manager'],
+                                 in_message=new_message().input_for_put,
+                                 out_message=new_message().output_for_put)
+
+    @timeit
+    @check.need_personate_header(check.PERSONATE_ADMIN)
+    @audit_access
+    def delete(self, *args, **kwargs):
+        return new_handler().delete(manager=kwargs['apisvc_res_manager'],
+                                    in_message=new_message().input_for_delete,
+                                    out_message=new_message().output_for_delete)
+
+
+API.add_resource(Tenant, '/v2/admin/nodes')
