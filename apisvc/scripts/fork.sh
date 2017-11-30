525b2fcecffb42d86e9f456fffa036768257bf49
Jim Lin <jimlintw922@gmail.com>
2017-11-30 16:20:57 +0800

    add fork.sh

---
+++ apisvc/scripts/fork.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -e
+
+# Usage: $0 <new name>
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <new name>"
+    exit 1
+fi
+BRAND=$1
+
+# ==============================
+
+FILE=../README.md
+sed -i "s/apisvc/$BRAND/g" $FILE
+
+# ==============================
+
+FILE=../SOURCE.md
+sed -i "s/apisvc/$BRAND/g" $FILE
+
+# ==============================
+
+FILE=../setup.py
+sed -i "s/apisvc/$BRAND/g" $FILE
+
+# ==============================
+
+FILE=./bring-up-fresh-dev-env.sh
+sed -i "s/apisvc/$BRAND/g" $FILE
+
+# ==============================
+
+FILE=./clear-fs-cache.sh
+sed -i "s/apisvc/$BRAND/g" $FILE
+
+# ==============================
+
+FILE=./init-etcd-db-for-dev.sh
+sed -i "s/apisvc/$BRAND/g" $FILE
+
+# ==============================
+
+FILE=./init-fs-cache-for-dev.sh
+sed -i "s/apisvc/$BRAND/g" $FILE
+
+# ==============================
+
+# TODO APISVC -> $BRANDING
+# TODO apisvc -> $BRANDING
+FILE=../apisvc/*.py
+
+# ==============================
+
+FOLDER=../apisvc
+mv $FOLDER ../$BRAND
