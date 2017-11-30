734bbbb6e4a5e97236168fc27380ad9ae214aa9e
Jim Lin <jimlintw922@gmail.com>
2017-11-30 11:43:09 +0800

    coding admin api spec

--- apisvc/apisvc/routes/v2/admin/__init__.py
+++ apisvc/apisvc/routes/v2/admin/__init__.py
@@ -19,14 +19,18 @@ def v2_admin_apis():
                        'data': [{'method': 'GET', 'path': '/v2/admin/healthz'},
                                 {'method': 'GET', 'path': '/v2/admin/apis'},
                                 {'method': 'GET', 'path': '/v2/admin/nodes'},
-                                {'method': 'PUT', 'path': '/v2/admin/nodes'},
+                                {'method': 'GET', 'path': '/v2/admin/nodes/compute'},
+                                {'method': 'GET', 'path': '/v2/admin/nodes/:id'},
+                                {'method': 'PUT', 'path': '/v2/admin/nodes/:id'},
                                 {'method': 'GET', 'path': '/v2/admin/pools'},
                                 {'method': 'POST', 'path': '/v2/admin/pools'},
-                                {'method': 'PUT', 'path': '/v2/admin/pools'},
-                                {'method': 'DELETE', 'path': '/v2/admin/pools'},
+                                {'method': 'GET', 'path': '/v2/admin/pools/:id'},
+                                {'method': 'PUT', 'path': '/v2/admin/pools/:id'},
+                                {'method': 'DELETE', 'path': '/v2/admin/pools/:id'},
                                 {'method': 'GET', 'path': '/v2/admin/rings'},
                                 {'method': 'POST', 'path': '/v2/admin/rings'},
-                                {'method': 'PUT', 'path': '/v2/admin/rings'},
-                                {'method': 'DELETE', 'path': '/v2/admin/rings'}
+                                {'method': 'GET', 'path': '/v2/admin/rings/:id'},
+                                {'method': 'PUT', 'path': '/v2/admin/rings/:id'},
+                                {'method': 'DELETE', 'path': '/v2/admin/rings/:id'}
                                 ]}), \
            {'Content-Type': 'application/json'}
