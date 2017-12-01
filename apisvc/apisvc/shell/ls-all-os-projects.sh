9db95ff34edb09006ec76000406dd7b6a4f4c267
Jim Lin <jimlintw922@gmail.com>
2017-12-01 10:23:55 +0800

    add os/project_list.ipynb

---
+++ apisvc/apisvc/shell/ls-all-os-projects.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# package dependency
+# - openstack
+
+# sample usage
+# bash $0 /tmp ../../samples/ca.crt ../../samples/ca.key user1 role1
+# bash $0 /tmp ../../samples/ca.crt ../../samples/ca.key cclin system:masters
+
+set -e
+
+# input
+TMP=$1
+CA_CRT=$2
+CA_KEY=$3
+CN=$4
+O=$5
+
+# output
+KEY=$TMP/server.key
+CSR=$TMP/server.csr
+CRT=$TMP/server.crt
+LOG=$TMP/$(basename $0).log
+
+openssl req -new -sha256 -keyout $KEY -out $CSR -days 3650 -newkey rsa:2048 -nodes -subj "/O=$O/CN=$CN" > $LOG 2>&1
+openssl x509 -req -days 3650 -sha1 -CA $CA_CRT  -CAkey $CA_KEY -CAcreateserial -in $CSR -out $CRT > $LOG 2>&1
+
+jq -n "{
+    \"crt\": \"$(base64 $CRT)\",
+    \"key\": \"$(base64 $KEY)\"
+}"
+
+[ -f $KEY ] && rm $KEY
+[ -f $CSR ] && rm $CSR
+[ -f $CRT ] && rm $CRT
+[ -f $LOG ] && rm $LOG
