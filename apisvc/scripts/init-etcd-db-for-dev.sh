925a5cc009721306898c3de073cf495197537b58
Jim Lin <jimlintw922@gmail.com>
2017-12-04 11:14:23 +0800

    update init-etcd-db-for-dev.sh

--- apisvc/scripts/init-etcd-db-for-dev.sh
+++ apisvc/scripts/init-etcd-db-for-dev.sh
@@ -7,20 +7,26 @@ set -e
 
 export ETCDCTL_API=3
 
-echo -n "$(date) | INFO | put /apisvc/ "
-etcdctl put /apisvc/ ok
+# ================== level 1 ======================
 
-echo -n "$(date) | INFO | put /apisvc/ca/ "
-etcdctl put /apisvc/ca/ ok
+echo -n "$(date) | INFO | put /apisvc "
+etcdctl put /apisvc ok
 
-echo -n "$(date) | INFO | put /apisvc/ca/key "
-cat ../samples/ca.key | etcdctl put /apisvc/ca/key
+# ================== level 2 ======================
 
-echo -n "$(date) | INFO | put /apisvc/ca/crt "
-cat ../samples/ca.crt | etcdctl put /apisvc/ca/crt
+echo -n "$(date) | INFO | put /apisvc/controllers "
+etcdctl put /apisvc/controllers ok
 
-echo -n "$(date) | INFO | put /apisvc/controllers/ "
-etcdctl put /apisvc/controllers/ ok
+echo -n "$(date) | INFO | put /apisvc/nodes "
+etcdctl put /apisvc/nodes ok
+
+echo -n "$(date) | INFO | put /apisvc/rings "
+etcdctl put /apisvc/rings ok
+
+echo -n "$(date) | INFO | put /apisvc/ca "
+etcdctl put /apisvc/ca ok
+
+# ================== level 3 ======================
 
 echo -n "$(date) | INFO | put /apisvc/controllers/k8s "
 cat ../samples/controller.k8s.yaml | etcdctl put /apisvc/controllers/k8s
@@ -28,32 +34,36 @@ cat ../samples/controller.k8s.yaml | etcdctl put /apisvc/controllers/k8s
 echo -n "$(date) | INFO | put /apisvc/controllers/os "
 cat ../samples/controller.os.yaml | etcdctl put /apisvc/controllers/os
 
-echo -n "$(date) | INFO | put /apisvc/nodes/ "
-etcdctl put /apisvc/nodes/ ok
+echo -n "$(date) | INFO | put /apisvc/nodes/compute "
+etcdctl put /apisvc/nodes/compute ok
 
-echo -n "$(date) | INFO | put /apisvc/nodes/compute/ "
-etcdctl put /apisvc/nodes/compute/ ok
+echo -n "$(date) | INFO | put /apisvc/ca/key "
+cat ../samples/ca.key | etcdctl put /apisvc/ca/key
+
+echo -n "$(date) | INFO | put /apisvc/ca/crt "
+cat ../samples/ca.crt | etcdctl put /apisvc/ca/crt
+
+echo -n "$(date) | INFO | put /apisvc/rings/admin "
+etcdctl put /apisvc/rings/admin ok
+
+echo -n "$(date) | INFO | put /apisvc/rings/tenant "
+etcdctl put /apisvc/rings/tenant ok
+
+echo -n "$(date) | INFO | put /apisvc/rings/user "
+etcdctl put /apisvc/rings/user ok
+
+# ================== level 4 ======================
 
 echo -n "$(date) | INFO | put /apisvc/nodes/compute/comp1 "
-etcdctl put /apisvc/nodes/compute/comp1 TBD
+cat ../samples/node.compute.comp1.json | etcdctl put /apisvc/nodes/compute/comp1
 
 echo -n "$(date) | INFO | put /apisvc/nodes/compute/comp2 "
-etcdctl put /apisvc/nodes/compute/comp2 TBD
+cat ../samples/node.compute.comp2.json | etcdctl put /apisvc/nodes/compute/comp2
 
-echo -n "$(date) | INFO | put /apisvc/rings/ "
-etcdctl put /apisvc/rings/ ok
+echo -n "$(date) | INFO | put /apisvc/rings/admin/0000-0000-0000-0000 "
+etcdctl put /apisvc/rings/admin/0000-0000-0000-0000 ok
 
-echo -n "$(date) | INFO | put /apisvc/rings/admin/ "
-etcdctl put /apisvc/rings/admin/ ok
-
-echo -n "$(date) | INFO | put /apisvc/rings/tenant/ "
-etcdctl put /apisvc/rings/tenant/ ok
-
-echo -n "$(date) | INFO | put /apisvc/rings/user/ "
-etcdctl put /apisvc/rings/user/ ok
-
-echo -n "$(date) | INFO | put /apisvc/rings/admin/0000-0000-0000-0000/ "
-etcdctl put /apisvc/rings/admin/0000-0000-0000-0000/ ok
+# ================== level 5 ======================
 
 echo -n "$(date) | INFO | put /apisvc/rings/admin/0000-0000-0000-0000/k8s "
 cat ../samples/0000-0000-0000-0000.k8s.yaml | etcdctl put /apisvc/rings/admin/0000-0000-0000-0000/k8s
