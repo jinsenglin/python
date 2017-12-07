4eda1a48b231210b06faf6ab6ef4b278a54943cb
Jim Lin <jimlintw922@gmail.com>
2017-12-07 10:29:31 +0800

    update clear-fs-cache.sh

--- apisvc/scripts/clear-fs-cache.sh
+++ apisvc/scripts/clear-fs-cache.sh
@@ -2,7 +2,14 @@
 
 set -e
 
+echo "$(date) | INFO | clearing ../apisvc/cache/ca "
 rm -rf ../apisvc/cache/ca              # do not keep ca/
+
+echo "$(date) | INFO | clearing ../apisvc/cache/rings/admin/* "
 rm -rf ../apisvc/cache/rings/admin/*   # keep rings/admin/
+
+echo "$(date) | INFO | clearing ../apisvc/cache/rings/tenant/* "
 rm -rf ../apisvc/cache/rings/tenant/*  # keep rings/tenant/
+
+echo "$(date) | INFO | clearing ../apisvc/cache/rings/user/* "
 rm -rf ../apisvc/cache/rings/user/*    # keep rings/user/
