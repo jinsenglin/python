dcfbd74310400316e3734b31fd82cf5e88930107
Jim Lin <jimlintw922@gmail.com>
2017-12-07 13:52:53 +0800

    update Vagrantfile

--- apisvc/stage-site/start.sh
+++ apisvc/stage-site/start.sh
@@ -2,4 +2,9 @@
 
 set -e
 
+[ -d /var/cache/apisvc ] || cp -r /usr/lib/python2.7/site-packages/apisvc/cache /var/cache/apisvc
+[ -d /var/lock/apisvc ] || mkdir -p /var/lock/apisvc
+[ -d /var/log/apisvc ] || mkdir -p /var/log/apisvc
+[ -d /tmp/apisvc ] || mkdir -p /tmp/apisvc
+
 /bin/flask run -h 0.0.0.0 -p 5080 --no-reload --no-debugger --eager-loading --without-threads
