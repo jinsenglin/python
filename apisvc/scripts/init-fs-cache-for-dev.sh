236b39e75629ac03b87e1a9d5b2066133c86a65a
Jim Lin <jimlintw922@gmail.com>
2017-12-07 15:12:18 +0800

    update init-fs-cache-for-dev.sh

--- apisvc/scripts/init-fs-cache-for-dev.sh
+++ apisvc/scripts/init-fs-cache-for-dev.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+echo "$(date) | INFO | initing ../apisvc/cache/ca "
 [ -d ../apisvc/cache/ca/ ] || mkdir -p ../apisvc/cache/ca
 cp ../samples/ca.key ../apisvc/cache/ca/ca.key
 cp ../samples/ca.crt ../apisvc/cache/ca/ca.crt
 
+echo "$(date) | INFO | initing ../apisvc/cache/rings/admin/0000-0000-0000-0000 "
 [ -d ../apisvc/cache/rings/admin/0000-0000-0000-0000/ ] || mkdir -p ../apisvc/cache/rings/admin/0000-0000-0000-0000
 cp ../samples/0000-0000-0000-0000.k8s.yaml ../apisvc/cache/rings/admin/0000-0000-0000-0000/k8s.yaml
 cp ../samples/0000-0000-0000-0000.os.yaml ../apisvc/cache/rings/admin/0000-0000-0000-0000/os.yaml
