c3775161ded8053f7c3a077fa1865e7b1107c653
Jim Lin <jimlintw922@gmail.com>
2017-12-11 14:39:47 +0800

    update tests

--- apisvc/tests/managers/gm/test_create_pool.py
+++ apisvc/tests/managers/gm/test_create_pool.py
@@ -2,10 +2,10 @@ import unittest
 
 
 class TestGmMethods(unittest.TestCase):
-    def test_create_ring(self):
+    def test_create_pool(self):
         import apisvc
         gm = apisvc.managers.gm.Manager(role='admin', account='0000-0000-0000-0000')
-        result = gm.create_ring(tenant_id='admin', account_id='u02', ring_type='user')
+        result = gm.create_pool(tenant_id='t01')
         print(result)
 
 
