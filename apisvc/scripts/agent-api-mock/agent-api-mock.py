e95a4abc2e689113b7d0a23301be0d03fbe0da92
Jim Lin <jimlintw922@gmail.com>
2017-12-19 15:02:31 +0800

    update agent-api-mock.py

--- apisvc/scripts/agent-api-mock/agent-api-mock.py
+++ apisvc/scripts/agent-api-mock/agent-api-mock.py
@@ -1,9 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/v1/healthz")
+def healthz():
+    return "ok"
+
+@app.route("/v1/role/os", methods=['PUT'])
+def role_os():
+    return '{"status": 200}'
+
+@app.route("/v1/role/k8s", methods=['PUT'])
+def role_k8s():
+    return '{"status": 200}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=40080, debug=True)
