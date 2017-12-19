9c3f80afddaec3d600281ee8b89fcb87cbdfe015
Jim Lin <jimlintw922@gmail.com>
2017-12-19 13:44:11 +0800

    rename managers

--- apisvc/apisvc/managers/gm.py
+++ apisvc/apisvc/managers/gm.py
@@ -1,11 +1,11 @@
 import yaml
 from apisvc.common import util
 from apisvc.common.cache import fs as CACHE
-from apisvc.managers import k8s
-from apisvc.managers import os
-from apisvc.managers import fbi
-from apisvc.managers import cia
-from apisvc.managers import ninja
+from apisvc.managers import k8sm
+from apisvc.managers import osm
+from apisvc.managers import dbm
+from apisvc.managers import am
+from apisvc.managers import shellm
 from apisvc.common.log import LOGGER
 
 
@@ -34,14 +34,14 @@ class Manager(object):
             self._ca_crt_path, self._ca_key_path = CACHE.get_ca_pem_keys()
 
         # init managers
-        self._k8s_mgr = k8s.Manager(credential_path=self._k8s_credential_path)
-        self._os_mgr = os.Manager(credential_path=self._os_credential_path)
-        self._fbi_mgr = fbi.Manager()
-        self._cia_mgr = cia.Manager()
-        self._ninja_mgr = ninja.Manager(k8s_credential_path=self._k8s_credential_path,
-                                        os_credential_path=self._os_credential_path,
-                                        ca_crt_path=self._ca_crt_path,
-                                        ca_key_path=self._ca_key_path)
+        self._k8sm = k8sm.Manager(credential_path=self._k8s_credential_path)
+        self._osm = osm.Manager(credential_path=self._os_credential_path)
+        self._dbm = dbm.Manager()
+        self._am = am.Manager()
+        self._shellm = shellm.Manager(k8s_credential_path=self._k8s_credential_path,
+                                      os_credential_path=self._os_credential_path,
+                                      ca_crt_path=self._ca_crt_path,
+                                      ca_key_path=self._ca_key_path)
 
     def __str__(self):
         return '{0} {1}'.format(self._role, self._account)
@@ -70,20 +70,20 @@ class Manager(object):
     # ===================================== #
 
     def get_nodes(self, node_filter):
-        return self._fbi_mgr.get_nodes(node_filter=node_filter)
+        return self._dbm.get_nodes(node_filter=node_filter)
 
     def get_node(self, node_id, node_roles):
-        return self._fbi_mgr.get_node(node_id=node_id, node_roles=node_roles)
+        return self._dbm.get_node(node_id=node_id, node_roles=node_roles)
 
     def update_node(self, node_id, node_role, node_action):
         if node_role == 'compute' and node_action in ['from_os_to_k8s', 'from_k8s_to_os']:
-            node = self._fbi_mgr.get_node(node_id=node_id, node_roles=['compute'])
+            node = self._dbm.get_node(node_id=node_id, node_roles=['compute'])
             node = yaml.load(node['result']['compute'])
             if node_action == 'from_os_to_k8s':
-                return self._cia_mgr.switch_compute_node_from_os_to_k8s(node=node)
+                return self._am.switch_compute_node_from_os_to_k8s(node=node)
             else:
                 # i.e. node_action == 'from_k8s_to_os':
-                return self._cia_mgr.switch_compute_node_from_k8s_to_os(node=node)
+                return self._am.switch_compute_node_from_k8s_to_os(node=node)
 
         return {'result': {}}
 
@@ -94,16 +94,16 @@ class Manager(object):
     # ===================================== #
 
     def get_pools(self):
-        return self._fbi_mgr.get_rings(ring_filter='tenant')
+        return self._dbm.get_rings(ring_filter='tenant')
 
     def create_pool(self, tenant_id):
         # create k8s ns
-        k8s_namespace = self._ninja_mgr.create_k8s_namespace(tenant_id=tenant_id)
-        self._put_rollback(self._ninja_mgr.delete_k8s_namespace, tenant_id=tenant_id)
+        k8s_namespace = self._shellm.create_k8s_namespace(tenant_id=tenant_id)
+        self._put_rollback(self._shellm.delete_k8s_namespace, tenant_id=tenant_id)
 
         # create os project
-        os_project = self._ninja_mgr.create_os_project(tenant_id=tenant_id)
-        self._put_rollback(self._ninja_mgr.delete_os_project, tenant_id=tenant_id)
+        os_project = self._shellm.create_os_project(tenant_id=tenant_id)
+        self._put_rollback(self._shellm.delete_os_project, tenant_id=tenant_id)
 
         # create ring
         self.create_ring(tenant_id=tenant_id, account_id=tenant_id, ring_type='tenant')
@@ -130,28 +130,28 @@ class Manager(object):
     # ===================================== #
 
     def get_rings(self, ring_filter):
-        return self._fbi_mgr.get_rings(ring_filter=ring_filter)
+        return self._dbm.get_rings(ring_filter=ring_filter)
 
     def create_ring(self, tenant_id, account_id, ring_type):
         # create os user
-        os_user = self._ninja_mgr.create_os_user(tenant_id=tenant_id, account_id=account_id)
-        self._put_rollback(self._ninja_mgr.delete_os_user, account_id=account_id)
+        os_user = self._shellm.create_os_user(tenant_id=tenant_id, account_id=account_id)
+        self._put_rollback(self._shellm.delete_os_user, account_id=account_id)
 
         # create k8s user
-        k8s_user = self._ninja_mgr.create_k8s_user(tenant_id=tenant_id, account_id=account_id)
+        k8s_user = self._shellm.create_k8s_user(tenant_id=tenant_id, account_id=account_id)
 
         # create ring
-        k8s_controller = self._fbi_mgr.get_controller('k8s')
-        os_controller = self._fbi_mgr.get_controller('os')
+        k8s_controller = self._dbm.get_controller('k8s')
+        os_controller = self._dbm.get_controller('os')
 
         k8s_credential = util.native_k8s_user_object_to_ring_credential(k8s_controller=k8s_controller, k8s_user=k8s_user, k8s_ns=tenant_id)
         os_credential = util.native_os_user_object_to_ring_credential(os_controller=os_controller, os_user=os_user)
 
-        ring = self._fbi_mgr.create_ring(ring_type=ring_type,
+        ring = self._dbm.create_ring(ring_type=ring_type,
                                          account_id=account_id,
                                          k8s_credential=k8s_credential,
                                          os_credential=os_credential)
-        self._put_rollback(self._fbi_mgr.delete_ring, ring_type=ring_type, account_id=account_id)
+        self._put_rollback(self._dbm.delete_ring, ring_type=ring_type, account_id=account_id)
 
         return {'result': {'os_user': os_user, 'k8s_user': k8s_user, 'ring': ring}}
 
