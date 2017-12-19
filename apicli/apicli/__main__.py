118f0907bd7edc3e0ab84ee15128845d93c08838
Jim Lin <jimlintw922@gmail.com>
2017-12-19 17:56:24 +0800

    update main

--- apicli/apicli/__main__.py
+++ apicli/apicli/__main__.py
@@ -1,8 +1,11 @@
 from parsers.parser2 import parser
+from httpclient import client
 
 
 def main():
-    print (parser.parse_args())
+    args = parser.parse_args()
+    print (args)
+    client.post(cmd=args.subparser_name, arg=args.command)
 
 
 if __name__ == '__main__':
