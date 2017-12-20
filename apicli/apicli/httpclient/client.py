810d4b482b2f69e6e4f1a6992ddd88466e484b9d
Jim Lin <jimlintw922@gmail.com>
2017-12-20 17:38:33 +0800

    update httpclient to handle exceptions

--- apicli/apicli/httpclient/client.py
+++ apicli/apicli/httpclient/client.py
@@ -1,12 +1,48 @@
 import requests
+import simplejson
 
 
 def put(endpoint, role, user, cmd, arg):
     url = '{0}/v2/admin/proxy/{1}'.format(endpoint, cmd)
     headers = {'X-PERSONATE': '{0} {1}'.format(role, user)}
     payload = {'arg': arg.split()}
+
     try:
-        result = requests.put(url=url, headers=headers, json=payload)
-        print(result.json()['result'])
+        r = requests.put(url=url, headers=headers, json=payload, allow_redirects=True)
     except requests.exceptions.ConnectionError:
         print('ERR_CONN_REFUSED')
+        return
+
+    if r.status_code == requests.codes.ok:
+        try:
+            result = r.json()['result']
+        except simplejson.errors.JSONDecodeError:
+            print('ERR_BAD_RESPONSE :: JSONDecodeError')
+            return
+        except TypeError:
+            print('ERR_BAD_RESPONSE :: TypeError')
+            return
+        except KeyError:
+            print('ERR_BAD_RESPONSE :: KeyError')
+            return
+
+        print(result)
+        return
+    elif r.status_code == requests.codes.bad_request:
+        print('ERR_BAD_REQUEST')
+        return
+    elif r.status_code == requests.codes.unauthorized:
+        print('ERR_UNAUTHENTICATED')
+        return
+    elif r.status_code == requests.codes.forbidden:
+        print('ERR_UNAUTHORIZED')
+        return
+    elif r.status_code == requests.codes.not_found:
+        print('ERR_NOT_FOUND')
+        return
+    elif r.status_code == requests.codes.internal_server_error:
+        print('ERR_INTERNAL_SERVER_ERROR')
+        return
+    else:
+        print('ERR_OTHER_ERROR :: Status code = {0}'.format(r.status_code))
+        return
