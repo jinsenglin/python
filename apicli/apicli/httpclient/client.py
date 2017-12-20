d5101c8dad9bc37c97f00ca0a9a4e4973422d563
Jim Lin <jimlintw922@gmail.com>
2017-12-20 13:48:46 +0800

    update httpclient

--- apicli/apicli/httpclient/client.py
+++ apicli/apicli/httpclient/client.py
@@ -5,5 +5,8 @@ def put(endpoint, role, user, cmd, arg):
     url = '{0}/v2/admin/proxy/{1}'.format(endpoint, cmd)
     headers = {'X-PERSONATE': '{0} {1}'.format(role, user)}
     payload = {'arg': arg.split()}
-    result = requests.put(url=url, headers=headers, json=payload)
-    print(result.json()['result'])
+    try:
+        result = requests.put(url=url, headers=headers, json=payload)
+        print(result.json()['result'])
+    except requests.exceptions.ConnectionError:
+        print('ERR_CONN_REFUSED')
