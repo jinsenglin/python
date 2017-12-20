ba9895cb5d47fabed8ba19ca23e2e66c56278ab3
Jim Lin <jimlintw922@gmail.com>
2017-12-20 17:49:27 +0800

    update bring-up-local-os-keystone.sh to support https

--- apisvc/scripts/bring-up-local-os-keystone.sh
+++ apisvc/scripts/bring-up-local-os-keystone.sh
@@ -3,13 +3,14 @@
 set -e
 
 APISVC_OS_HOST=${APISVC_OS_HOST:-127.0.0.1}
+APISVC_OS_HTTPS=${APISVC_OS_HTTPS:-false}
 
 echo -n "$(date) | INFO | bringing up openstack keystone v9.1.0 (identity v3) "
 
 if [ $APISVC_OS_HOST == "127.0.0.1" ]; then
-    docker run --rm -d -p 5000:5000 -p 35357:35357 --name os-keystone -e HOSTNAME=$APISVC_OS_HOST stephenhsu/keystone:9.1.0
+    docker run --rm -d -p 5000:5000 -p 35357:35357 --name os-keystone -e HOSTNAME=$APISVC_OS_HOST -e TLS_ENABLED=$APISVC_OS_HTTPS stephenhsu/keystone:9.1.0
 else
-    docker run --rm -d -p 5000:5000 -p 35357:35357 --name os-keystone --net mynet --ip $APISVC_OS_HOST -h os-keystone stephenhsu/keystone:9.1.0
+    docker run --rm -d -p 5000:5000 -p 35357:35357 --name os-keystone --net mynet --ip $APISVC_OS_HOST -h os-keystone -e TLS_ENABLED=$APISVC_OS_HTTPS stephenhsu/keystone:9.1.0
 fi
 
 echo "$(date) | DEBUG | APISVC_OS_HOST= $APISVC_OS_HOST "
